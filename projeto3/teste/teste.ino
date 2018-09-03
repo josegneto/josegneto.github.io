0d5c9a0469bd0b24ea5acb7d5bb0bb5339dbace3
José Neto <zecorreianeto98@gmail.com>
2018-09-03 15:43:52 +0100

    matriz

--- projeto3/teste/teste.ino
+++ projeto3/teste/teste.ino
@@ -1,17 +1,18 @@
-// 2-dimensional array of row pin numbers:
+/* Teste da matrix 8x8 por josegneto.github.io */
+
+// Vetor com os pinos relativos as linhas
 int Row[] = {2,7,A5,5,13,A4,12,A2};
-// 2-dimensional array of column pin numbers:
+// Vetor com os pinos relativos as colunas
 int Column[] = {6,11,10,3,A3,4,8,9};
 
-unsigned char all[8][8] =     {1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1};
+// Cada um dos LEDs está a '1', ou seja, HIGH
+unsigned char all[8][8] = {1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1};
 
 void setup()
 {
-    Serial.begin(9600);
-
-    // iterate over the pins:
+    // Percorre todos os pinos
     for(int i = 0; i<8; i++)
-        // initialize the output pins:
+    // Inicializa os pinos como outputs
     {
         pinMode(Row[i],OUTPUT);
         pinMode(Column[i],OUTPUT);
@@ -20,27 +21,22 @@ void setup()
 
 void loop()
 {
-    if (Serial.available() > 0)
-    {  
-      Display(all);
-    }
+    Display(all);    
 }
+
 void Display(unsigned char dat[8][8])
 {
-  for(int i = 0; i<100; i++)
-  {
     for(int c = 0; c<8; c++)
     {
-        digitalWrite(Column[c],LOW);//use thr column
-        //loop
+        digitalWrite(Column[c],LOW); 
+        
         for(int r = 0; r<8; r++)
         {
             digitalWrite(Row[r],dat[r][c]);
         }
         delay(1);
-        Clear();  //Remove empty display light
+        Clear();  
     }
-  }
 }
 
 void Clear()                               
